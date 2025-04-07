79856d8bc41d86f91e2c59da080510e597038d7c
SSR Tokyo <ravikantbedi5@gmail.com>
2025-04-07 21:05:36 +0530

    second  commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Hero from './components/hero.jsx'
+import About from './components/about.jsx'
+import NavBar from './components/NavBar.jsx'
+import Features from './components/Features.jsx'
+import Storry from './components/Storry.jsx'
+import Contanctt from './components/Contanctt.jsx'
+import Footerr from './components/Footerr.jsx'
+
+function App() {
+  return (
+   <main>
+     <div>
+      <main className="realtive min-h-screen w-screen overflow-x-hidden">
+      <NavBar/>
+      <Hero/>
+      <About/>
+      {/* <FFeatures/> */}
+      <Features/> 
+     <Storry/>
+     <Contanctt/>
+     <Footerr/>
+     </main>
+   </div>
+    
+   </main>
+  )
+}
+
+export default App
---
+++ src/components/FFeatures.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FFeatures() {
+  return (
+    <div>FFeatures</div>
+  )
+}
+
+export default FFeatures
